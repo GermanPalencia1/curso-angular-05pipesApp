f3b600201aa1679d41aef4fafe4f8d68585609bf
Germán <german.palencia@adentia.es>
2023-09-01 13:53:36 +0200

    uncommon page estructure

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './uncommon-page.component.html',
   styleUrls: ['./uncommon-page.component.css']
 })
+
 export class UncommonPageComponent {
+
+
 
 }
